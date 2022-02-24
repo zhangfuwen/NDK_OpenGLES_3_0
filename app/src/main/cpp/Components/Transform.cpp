6e204673448c1e2a9e2113361f6f23f52c2ee621
zhangfuwen <zhangfuwen@foxmail.com>
2022-02-24 21:37:54 +0800

    change build script

---
+++ app/src/main/cpp/Components/Transform.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zhangfuwen on 2022/2/24.
+//
+
+#include "Transform.h"
---
+++ app/src/main/cpp/Components/Transform.h
@@ -0,0 +1,14 @@
+//
+// Created by zhangfuwen on 2022/2/24.
+//
+
+#ifndef NDK_OPENGLES_3_0_TRANSFORM_H
+#define NDK_OPENGLES_3_0_TRANSFORM_H
+
+
+class Transform {
+
+};
+
+
+#endif //NDK_OPENGLES_3_0_TRANSFORM_H
