26dc5f901a649026fab2768767cc25df1a1ebb12
zhangfuwen <zhangfuwen@bytedance.com>
2022-03-16 10:57:17 +0800

    add glfw triangle

--- app/src/glfw/triangle.cpp
+++ app/src/glfw/triangle.cpp
@@ -18,7 +18,7 @@ const char *vertexShaderSource = "#version 310 es\n"
                                  "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
                                  "}\0";
 const char *fragmentShaderSource = "#version 310 es\n"
-                                   "out mediumpeses vec4 FragColor;\n"
+                                   "out mediump vec4 FragColor;\n"
                                    "void main()\n"
                                    "{\n"
                                    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
